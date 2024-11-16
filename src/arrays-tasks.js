ab8968451abc3a6b0a1a8e84a46c7735db50b1dd
Aleksandr <tsybulskialex@gmail.com>
2024-11-16 19:23:11 +0300

    feat: completed 30 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,13 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const count = end - start + 1;
+  const arr = Array(count).fill(1);
+  const str = arr.join('');
+  const newArr = Array.from(str);
+  const result = newArr.map((item, id) => start + id);
+  return result;
 }
 
 /**
