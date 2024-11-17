55795684e4d31e0c610da247204f5a1ab5f7a903
Aleksandr <tsybulskialex@gmail.com>
2024-11-17 15:12:16 +0300

    feat: completed 34 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -450,8 +450,14 @@ function getIdentityMatrix(/* n */) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const arr = numbers.map((item, id) => {
+    if (item % 2 === 0) {
+      return undefined;
+    }
+    return id;
+  });
+  return arr.filter((item) => item !== undefined);
 }
 
 /**
