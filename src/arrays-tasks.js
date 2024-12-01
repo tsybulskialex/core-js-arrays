f43dcc6e422652fa7dd64118cd3bb40886a7ec49
Aleksandr <tsybulskialex@gmail.com>
2024-12-01 10:08:33 +0300

    feat: completed 36 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -346,8 +346,13 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  return arr.reduce(
+    (pre, cur) => pre.reduce((a, b) => a - b) + cur.reduce((a, b) => a - b)
+  );
 }
 
 /**
