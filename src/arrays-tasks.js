3892cae9b56d4f3794d823f9f088716fd621c405
Aleksandr <tsybulskialex@gmail.com>
2024-12-01 15:20:21 +0300

    feat: completed 37 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -367,8 +367,11 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const newArr = arr.map((item, id) =>
+    arr.slice(chunkSize * id, chunkSize * id + chunkSize)
+  );
+  return newArr.filter((item) => item.length !== 0);
 }
 
 /**
